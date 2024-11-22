d9ba31a5f1604783ab70063260d52b2d302bcf02
nataliaclem <nataliaclementinosilva@gamil.com>
2024-11-21 23:09:18 -0300

    atualizações

---
+++ js/produtos.js
@@ -0,0 +1,10 @@
+   // Seleciona o botão pelo ID
+    const botaoComprar = document.getElementById("botaoComprar");
+
+    // Adiciona um evento de clique ao botão
+    botaoComprar.addEventListener("click", function () {
+      // Redireciona para a página de cadastro
+      window.location.href = "cadastro.html";
+    });
+  
+  
