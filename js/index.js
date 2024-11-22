07ab62429afcecace6886b5c0a30071b316b259b
nataliaclem <nataliaclementinosilva@gamil.com>
2024-11-22 13:34:21 -0300

    atualizações

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-const images = [
-  'https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png',
-  'https://a-static.mlcdn.com.br/450x450/coleira-para-cachorro-em-couro-ajustavel-reforcada-caes-pet-n-7-crepardi/123wepet1/2899/65509c8c57cdce4c99c5893134b12b7a.jpeg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYLLlv0gvUZBmDEskEM_DC2HNUO9I2UeCSXQ&s '
-];
-
-let currentIndex = 0;
-const bannerElement = document.querySelector('.banner');
-
-function updateBackgroundImage() {
-  bannerElement.style.backgroundImage = `url(${images[currentIndex]})`;
-}
-
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  updateBackgroundImage();
-}
-
-function prevImage() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  updateBackgroundImage();
-}
-
-// Inicializa com a primeira imagem
-updateBackgroundImage();
-
-// Se quiser mudar automaticamente as imagens a cada 5 segundos:
-setInterval(nextImage, 5000);
-
 
 /* ---------------------------------------------------------------- */
 
